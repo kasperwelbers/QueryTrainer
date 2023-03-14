b941a92ffc346dd56a4491cc8697e351da5ac5e8
Kasper Welbers <kasperwelbers@gmail.com>
2023-03-14 10:49:52 +0100

    update data

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,18 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+import { Inter } from "next/font/google";
+
+const font = Inter({ subsets: ["latin"] });
+
 export default function App({ Component, pageProps }: AppProps) {
   const [queryClient] = React.useState(() => new QueryClient());
 
   return (
     <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />
+      <main className={font.className}>
+        <Component {...pageProps} />
+      </main>
     </QueryClientProvider>
   );
 }
